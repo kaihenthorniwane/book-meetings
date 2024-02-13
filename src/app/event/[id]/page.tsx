6a7aad4becb61d2ac6de7d10f8e57cd5cad1e991
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2024-02-12 20:07:57 -0800

    use unstable_cache

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -6,6 +6,11 @@ import {
   getDominantColor,
   getRelativeLuminance,
 } from "@/util/sampleColor";
+import { unstable_cache } from "next/cache";
+
+const getCachedDominantColor = unstable_cache(getDominantColor);
+
+const getCachedAverageTopColor = unstable_cache(getAverageTopColor);
 
 export type ParagraphObject = {
   text: string;
@@ -65,13 +70,13 @@ export default async function Page({ params }: { params: { id: string } }) {
     splitStringByNewlineToObjects(rawDescription);
 
   const imageUrl: string = parsedData.image;
-  const dominantColor: string = await getDominantColor(imageUrl);
+  const dominantColor: string = await getCachedDominantColor(imageUrl);
   const colorLuminance: number = getRelativeLuminance(dominantColor);
   const isTooDark: boolean = colorLuminance < 0.5;
   const defaultTextColor = isTooDark
     ? "var(--Brand-White)"
     : "var(--Brand-Black)";
-  const averageImageTopColor: string = await getAverageTopColor(imageUrl);
+  const averageImageTopColor: string = await getCachedAverageTopColor(imageUrl);
 
   return (
     <EventContent
