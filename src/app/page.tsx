e6a9bb928c8d4d420b64d082632cd4c8e63d7e0f
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2024-02-18 19:37:46 -0800

    Format desktop page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,26 @@ export default function Home() {
 
   return (
     <DefaultLayout>
-      <div className="flex flex-col gap-4">
-        <span className="font-semibold">This is a temporary homepage</span>
-        {events.map((event) => (
-          <div key={event.id}>
-            <div>{event.name}</div>
-            <div>{event.tagline}</div>
-            <Link className="underline" href={"/event/" + event.id}>
-              Page
-            </Link>
-          </div>
-        ))}
+      <div className="flex flex-col gap-10">
+        <div className="flex flex-col gap-3">
+          <span className="text-4xl">Available Mentoring Sessions</span>
+          <span className="max-w-screen-md">
+            From an one-on-one introduction to React's basics all the way up to
+            a deep dive into state mechanics - we got just the right session for
+            you!
+          </span>
+        </div>
+        <div className="md:grid md:grid-cols-3 md:gap-10 flex flex-col gap-5">
+          {events.map((event) => (
+            <div key={event.id}>
+              <div>{event.name}</div>
+              <div>{event.tagline}</div>
+              <Link className="underline" href={"/event/" + event.id}>
+                Page
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
     </DefaultLayout>
   );
--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -4,7 +4,7 @@ export default function DefaultLayout(props: { children: ReactNode }) {
   return (
     <body className="text-xl font-body text-brandBlack">
       <main className="flex flex-col gap-4 items-center">
-        <div className="flex flex-col w-full max-w-screen-sm p-5">
+        <div className="flex flex-col md:w-auto md:max-w-screen-xl md:mt-16 md:mx-10 md:p-0 p-5 w-full">
           {props.children}
         </div>
       </main>
