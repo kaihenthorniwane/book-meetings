09ffa21c31381b9ec340a723014c87e0555f070a
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2024-02-18 19:03:19 -0800

    update data

--- src/store/dataStore.ts
+++ src/store/dataStore.ts
@@ -20,7 +20,7 @@ let events: Event[] = [
     name: "Kickstart with React: Personal Intro",
     tagline: "Tailored guidance for total beginners in React.",
     description:
-      "Starting with React can be daunting.\n\nYet, with the right guidance, it can be an exhilarating journey.\n\nIn this session, we will begin by demystifying React's core philosophy.\n\nWhy was React created?\n\nWhat problems does it solve in the world of web development?\n\nTogether, we'll delve into the virtual DOM, JSX, and component-based architecture.\n\nNo need to worry about the jargon; I'll break everything down step by step.\n\nWe'll touch on the importance of unidirectional data flow and the component lifecycle.\n\nWe'll set up a new React project together.\n\nBy the end of our session, you will have a clear understanding and a roadmap tailored just for you, to aid your React journey.",
+      "Starting with React can be daunting.\n\nYet, with the right guidance, it can be an exhilarating journey.\n\nIn this session, we will begin by demystifying React's core philosophy.\n\nWhy was React created?\n\nWhat problems does it solve in the world of web development?\n\nTogether, we'll delve into the virtual DOM, JSX, and component-based architecture.\n\nBy the end of our session, you will have a clear understanding and a roadmap tailored just for you, to aid your React journey.",
     image:
       "https://uploads-ssl.webflow.com/61f4510e8113b1209cc00ab0/65cab5655af2fcdc235c879b_img2.webp",
     date: new Date("2023-10-31"),
