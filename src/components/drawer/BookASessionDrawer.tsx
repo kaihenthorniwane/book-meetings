d1dfba93e022bf3eb73fecd2eaaf4212a2ab657c
kaihenthorniwane <55228611+kaihenthorniwane@users.noreply.github.com>
2024-03-08 18:15:32 -0800

    fix padding

--- src/components/drawer/BookASessionDrawer.tsx
+++ src/components/drawer/BookASessionDrawer.tsx
@@ -17,7 +17,7 @@ export default function BookASessionDrawer({
 }: BookASessionDrawerProps) {
   return (
     <>
-      <form className="fixed left-0 right-0 bottom-0 flex flex-col px-5 py-7 bg-brandWhite rounded-t-3xl gap-7 text-brandBlack z-50">
+      <form className="fixed left-0 right-0 bottom-0 flex flex-col px-5 pt-7 pb-5 bg-brandWhite rounded-t-3xl gap-7 text-brandBlack z-50">
         <div className="flex flex-col gap-3">
           <span className="text-4xl">Book A Session</span>
           <div className="flex gap-4 font-medium">
